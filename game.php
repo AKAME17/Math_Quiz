fd39164cbaa47efd27c312939506dbe76bdba2f5
AKAME17 <nelyzagalia21@gmail.com>
2024-11-26 10:19:37 +0800

    adjustment and error handling

--- game.php
+++ game.php
@@ -1,26 +1,26 @@
 <?php 
 session_start();
 
-if ($SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['start_game'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['start_game'])) {
     $_SESSION['operator'] = $_POST['operator'];
     $_SESSION['level'] = $_POST['level'];
     $_SESSION['test_count'] = $_POST['test_count'];
     $_SESSION['current_test'] = 0;
-    $_SESSION['correct_answer'] = 0;
-    $_SESSION['wrong_answer'] = 0;
+    $_SESSION['correct_answers'] = 0;
+    $_SESSION['wrong_answers'] = 0;
 
-    header("locatio: game.php");
+    header("location: game.php");
     exit();
 }
 
-if ($_SERVER ['REQUEST_METHOD']=== 'post' && isset ($_POST['submit_answer'])) {
+if ($_SERVER ['REQUEST_METHOD']=== 'POST' && isset ($_POST['submit_answer'])) {
     $correct_answer = $_SESSION['correct_answer'];
-    $user_name = $_POST['answer'];
+    $user_answer= $_POST['answer'];
 
     if($user_answer == $correct_answer){
-        $_SESSION['correct_answer']++;
+        $_SESSION['correct_answers']++;
     } else {
-        $_SESSION['wrong_answer']++;
+        $_SESSION['wrong_answers']++;
     }
     $_SESSION['current_test']++;
 }
@@ -87,7 +87,7 @@ if (isset($_SESSION['current_test']) && $_SESSION['current_test'] < $_SESSION['t
         <button type="submit" name="submit_answer">Submit Answer</button>
     </form>
 
-    <?php
+<?php
 } elseif (isset($_SESSION['current_test'])) {
     
     $correct = $_SESSION['correct_answers'];
